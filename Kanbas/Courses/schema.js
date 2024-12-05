1127f05bba8650c988321eb1a535eb7b3d708d50
yunma-code <ma.yun2@northeastern.edu>
2024-12-05 15:38:07 -0500

    a6 5.1

---
+++ Kanbas/Courses/schema.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+	{
+		name: String,
+		number: String,
+		credits: Number,
+		description: String,
+	},
+	{ collection: "courses" }
+);
+export default courseSchema;
