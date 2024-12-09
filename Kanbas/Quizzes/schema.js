d932a9ed23c3b8d53f80e198fe4babcce0de90ba
yunma-code <ma.yun2@northeastern.edu>
2024-12-09 14:02:42 -0500

    update quiz schema

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -3,6 +3,27 @@ import mongoose from "mongoose";
 const quizSchema = new mongoose.Schema(
 	{
 		/* add quiz attributes */
+		course: String,
+		title: String,
+		points_possible: Number,
+		quiz_type: String,
+		assignment_group_id: String,
+		assignment_group_type: String,
+		shuffle_answers: Boolean,
+		allowed_attempts: Boolean,
+		attempts_number: Number,
+		show_correct_answers: Boolean,
+		one_question_at_a_time: Boolean,
+		has_access_code: Boolean,
+		require_lockdown_browser: Boolean,
+		cant_go_back: Boolean,
+		due_at: String,
+		unlock_at: String,
+		lock_at: String,
+		description: String,
+		time_limit: Number,
+		published: Boolean,
+		questions: Array,
 		quiz: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
 	},
 	{ collection: "quizzes" }
